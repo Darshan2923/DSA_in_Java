83d6958c03343398d4b9102df94e509b20df6c61
Darshan <darshanpatel2923@gmail.com>
2024-12-13 08:28:07 +0530

    Adjustments in package

--- com/arrays_strings/CheckPermutation.java
+++ com/arrays_strings/CheckPermutation.java
@@ -1,5 +1,7 @@
+package com.arrays_strings;
+
 public class CheckPermutation {
     public static void main(String[] args) {
-        System.out.println("Permutation code here!!!");
+        System.out.println("Check permutation code!!");
     }
 }
--- com/arrays_strings/isUnique.java
+++ com/arrays_strings/isUnique.java
@@ -1,5 +1,7 @@
+package com.arrays_strings;
+
 public class IsUnique {
     public static void main(String[] args) {
-        System.out.println("Is Unique string code here!");
+        System.out.println("Is Unique code");
     }
 }
